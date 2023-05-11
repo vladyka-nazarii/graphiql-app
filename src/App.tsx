8a927d7b5ef0be24bef483c90592196189bc81ee
Eugene <gradam2001@gmail.com>
2023-05-11 19:11:15 +0300

    GQL-11 feat(auth): add loading for main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
 import { AppRoutes } from './routes/AppRoutes';
+import { useLoadUser } from './hooks/useLoadUser';
+import { CircularProgress } from '@mui/material';
 
-export const App = () => (
-  <>
-    <Header />
-    <main>
-      <AppRoutes />
-    </main>
-    <Footer />
-  </>
-);
+export const App = () => {
+  const loading = useLoadUser();
+  return (
+    <>
+      <Header />
+      <main>{(loading && <CircularProgress />) || <AppRoutes />}</main>
+      <Footer />
+    </>
+  );
+};
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,27 +1,13 @@
 import { Navigate, Route, Routes } from 'react-router';
-import { useEffect } from 'react';
 
 import { Main } from '../pages/Main/Main';
 import { NotFound } from '../pages/NotFound/NotFound';
 import { useAuth } from '../hooks/useAuth';
-import { useAppDispatch } from '../hooks/redux-hooks';
-import { auth } from '../firebase/firebase-config';
-import { onAuthStateChanged } from 'firebase/auth';
-import { setUser } from '../redux/slices/userSlice';
 import { Login } from '../pages/Login/Login';
 import { Register } from '../pages/Register/Register';
 
 export const AppRoutes = () => {
   const { isAuth } = useAuth();
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        dispatch(setUser({ email: user.email, id: user.uid, token: user.refreshToken }));
-      }
-    });
-  }, [dispatch]);
 
   return (
     <Routes>
