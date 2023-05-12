d80a5f59a4527e41f7adf57f95cb2b4bbd2d4797
Eugene <gradam2001@gmail.com>
2023-05-12 12:03:47 +0300

    GQL-9 feat(header): add sticky header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
-import { Button } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { AppBar, Button, Container, Toolbar, Typography } from '@mui/material';
+
 import { useAuth } from '../../hooks/useAuth';
-import styles from './Header.module.css';
 import { signOutUser } from '../../firebase/sign-out-user';
 import { useAppDispatch } from '../../hooks/redux-hooks';
 import { removeUser } from '../../redux/slices/userSlice';
@@ -8,20 +9,42 @@ import { removeUser } from '../../redux/slices/userSlice';
 export const Header = () => {
   const { isAuth } = useAuth();
   const dispatch = useAppDispatch();
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const signOutHandler = async () => {
     await signOutUser();
     dispatch(removeUser());
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      if (scrollTop > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
-      <h2 className={styles.title}>Header</h2>
-      {isAuth && (
-        <Button color="primary" variant="contained" onClick={signOutHandler}>
-          Sign Out
-        </Button>
-      )}
-    </header>
+    <AppBar color={isScrolled ? 'secondary' : 'primary'} sx={{ transition: 'all 0.6s' }}>
+      <Container>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Typography variant="h6">GraphQl Playground</Typography>
+          {isAuth && (
+            <Button color="inherit" onClick={signOutHandler}>
+              Sign Out
+            </Button>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 };
