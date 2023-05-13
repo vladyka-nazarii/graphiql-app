6af334e3473be111392ed9bf9b5930f3dfbc0a60
Maksym Kuzmych <kuzmichmaksym1997@gmail.com>
2023-05-13 12:05:22 +0300

    GQL-8 feat(welcome-page): add welcome page

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router';
 
 import { Main } from '../pages/Main/Main';
+import { Welcome } from '../pages/Welcome/Welcome';
 import { NotFound } from '../pages/NotFound/NotFound';
 
 export const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Main />} />
+    <Route path="/welcome" element={<Welcome />} />
     <Route path="*" element={<NotFound />} />
   </Routes>
 );
