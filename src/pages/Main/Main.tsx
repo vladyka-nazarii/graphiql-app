228066208558ca1596a576e225efa8c3c2bc6dd4
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2023-05-14 14:00:37 +0300

    GQL-12 feat(layout): add play btn

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 
 import { Schema } from '../../components/Schema/Schema';
@@ -5,25 +6,40 @@ import { BasicTabs } from '../../components/Tabs/Tabs';
 
 import styles from './Main.module.scss';
 
-export const Main = () => (
-  <div className={styles.main}>
-    <div className={styles.query}>
+export const Main = () => {
+  const [request, setRequest] = useState(false);
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.query}>
+        <CodeMirror
+          value="console.log('hello world!');"
+          height="100%"
+          width="100%"
+          editable={true}
+          theme="light"
+        />
+        <BasicTabs />
+      </div>
       <CodeMirror
         value="console.log('hello world!');"
         height="100%"
         width="100%"
-        editable={true}
+        editable={false}
         theme="light"
       />
-      <BasicTabs />
+      <Schema />
+      <img
+        className={styles.play}
+        src={request ? './stop-button.svg' : './play-button.svg'}
+        alt="play"
+        onClick={() => {
+          setRequest((prev) => !prev);
+          setTimeout(() => {
+            setRequest((prev) => !prev);
+          }, 1000);
+        }}
+      />
     </div>
-    <CodeMirror
-      value="console.log('hello world!');"
-      height="100%"
-      width="100%"
-      editable={false}
-      theme="light"
-    />
-    <Schema />
-  </div>
-);
+  );
+};
