38074517439877156c012cda9fd7b9f026324784
Maksym <94698037+MaksymKuzmych@users.noreply.github.com>
2023-05-14 14:43:17 +0300

    GQL-8 feat(welcome-page): add welcome page (#4)
    * GQL-8 feat(welcome-page): add welcome page
    
    * GQL-8 fix(layout): add adaptive

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router';
 
 import { Main } from '../pages/Main/Main';
+import { Welcome } from '../pages/Welcome/Welcome';
 import { NotFound } from '../pages/NotFound/NotFound';
 import { useAuth } from '../hooks/useAuth';
 import { Login } from '../pages/Login/Login';
@@ -12,6 +13,7 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={isAuth ? <Main /> : <Navigate to="login" replace />} />
+      <Route path="/welcome" element={<Welcome />} />
       <Route path="/login" element={isAuth ? <Navigate to="/" replace /> : <Login />} />
       <Route path="/register" element={isAuth ? <Navigate to="/" replace /> : <Register />} />
       <Route path="*" element={<NotFound />} />
