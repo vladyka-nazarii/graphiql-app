0807d8f7cd0620e70f01f3d1fc8150b31bc658b6
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2023-05-15 12:41:09 +0300

    GQL-19 feat(apollo): add example query

--- src/components/Schema/Schema.tsx
+++ src/components/Schema/Schema.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_SCHEMA } from '../../apollo/schema';
 
 import styles from './Schema.module.scss';
 
 export const Schema = () => {
   const [open, setOpen] = useState(false);
+  const { loading, error, data } = useQuery(GET_SCHEMA);
 
   return (
     <>
@@ -13,7 +17,9 @@ export const Schema = () => {
       >
         SCHEMA
       </div>
-      <div className={`${styles.schema} ${open && styles.opened}`}>Here must be a Scheme</div>
+      <pre className={`${styles.schema} ${open && styles.opened}`}>
+        {loading ? 'Loading...' : error ? error.message : JSON.stringify(data, null, '\t')}
+      </pre>
     </>
   );
 };
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,39 +1,60 @@
 import { useState } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
+import { useQuery, gql } from '@apollo/client';
 
 import { Schema } from '../../components/Schema/Schema';
 import { BasicTabs } from '../../components/Tabs/Tabs';
-import { ResponseData } from '../../components/ResponseData/ResponseData';
 
 import styles from './Main.module.scss';
 
 export const Main = () => {
-  const [request, setRequest] = useState(false);
+  const queryExample = `query ExampleQuery {
+    company {
+      ceo
+    }
+    roadster {
+      apoapsis_au
+    }
+  }
+  `;
+  const [query, setQuery] = useState(queryExample);
+  const GET_DATA = gql`
+    ${query}
+  `;
+  const { loading, error, data } = useQuery(GET_DATA);
 
   return (
     <div className={styles.main}>
       <div className={styles.query}>
         <CodeMirror
-          value="console.log('hello world!');"
+          value={queryExample}
           height="100%"
           width="50vw"
           editable={true}
           theme="light"
+          onChange={(value: string) => setQuery(value)}
         />
         <BasicTabs />
       </div>
-      <ResponseData />
+      <CodeMirror
+        value={
+          loading
+            ? 'Loading...'
+            : error
+            ? `Error : ${error.message}`
+            : JSON.stringify(data, null, '\t')
+        }
+        height="calc(100vh - 64px - 64px)"
+        width="50vw"
+        editable={false}
+        theme="light"
+      />
       <Schema />
       <img
         className={styles.play}
-        src={request ? './stop-button.svg' : './play-button.svg'}
+        src={loading ? './stop-button.svg' : './play-button.svg'}
         alt="play"
-        onClick={() => {
-          setRequest((prev) => !prev);
-          setTimeout(() => {
-            setRequest((prev) => !prev);
-          }, 1000);
-        }}
+        onClick={() => {}}
       />
     </div>
   );
