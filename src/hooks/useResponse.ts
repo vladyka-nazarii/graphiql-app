465b4f44522b1914b39b7a6b7357cc9a4990556d
Nazarii Vladyka <vladyka.nazarii@gmail.com>
2023-05-17 14:31:41 +0300

    GQL-19 feat(apollo): add codegen

--- src/hooks/useResponse.ts
+++ src/hooks/useResponse.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, ServerError } from '@apollo/client';
 
 import { QUERY_EXAMPLE } from '../apollo/queryExample';
 import { queryValidation } from '../apollo/queryValidation';
@@ -20,36 +20,25 @@ export const useResponse = (dataValue: string) => {
     `,
   );
 
-  const correctData: typeof data = {};
-
-  const removeTypename = (sourceData: typeof data, correctData: typeof data) => {
-    if (typeof sourceData === 'object') {
-      Object.keys(sourceData).forEach((key) => {
-        if (typeof sourceData[key] === 'object') {
-          correctData[key] = {};
-          removeTypename(sourceData[key], correctData[key]);
-        } else {
-          if (key !== '__typename') {
-            correctData[key] = sourceData[key];
-          }
-        }
-      });
-    }
-  };
-
   if (!dataValue) {
     return { loading, data: '' };
   }
   if (validation === 'Error: Wrong query format!') {
-    return { loading, data: 'Wrong query format!' };
+    return { loading, data: 'Error: Wrong query format!' };
   }
   if (loading) {
     return { loading, data: 'Loading...' };
   }
   if (error) {
-    return { loading, data: `Error: ${error.message}` };
+    return {
+      loading,
+      data: `Error: ${error.message}\n${JSON.stringify(
+        (error.networkError as ServerError).result || 'Check your internet connection',
+        null,
+        '\t',
+      )}`,
+    };
   }
 
-  removeTypename(data, correctData);
-  return { loading, data: JSON.stringify(correctData, null, '\t') };
+  return { loading, data: JSON.stringify(data, null, '\t').replace(/.*__typename.*\n/g, '') };
 };
