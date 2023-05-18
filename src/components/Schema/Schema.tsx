67ff091a5255a19c26c2a4aff85299ee76f4d92c
Nazarii Vladyka <106691030+vladyka-nazarii@users.noreply.github.com>
2023-05-18 22:25:00 +0300

    GQL-19 Apollo Add Codegen (#8)
    * GQL-19 feat(apollo): add apollo
    
    * GQL-19 feat(apollo): add example query
    
    * GQL-19 feat(apollo): add play btn func
    
    * GQL-19 feat(apollo): remove __typename from response
    
    * GQL-19 feat(apollo): add codegen
    
    * GQL-19 feat(apollo): add useLazyQuery
    
    * GQL-19 fix(apollo): remove console.log
    
    * GQL-19 fix(apollo): empty data in useResponse
    
    * GQL-19 feat(apollo): add useLazyQuery to Shema.tsx
    
    * GQL-19 fix(apollo): remove ternary from Schema.tsx

--- src/components/Schema/Schema.tsx
+++ src/components/Schema/Schema.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { useQuery } from '@apollo/client';
+import { useMemo, useState } from 'react';
+import { useLazyQuery } from '@apollo/client';
 
 import { GET_SCHEMA } from '../../apollo/schema';
 
@@ -7,19 +7,32 @@ import styles from './Schema.module.scss';
 
 export const Schema = () => {
   const [open, setOpen] = useState(false);
-  const { loading, error, data } = useQuery(GET_SCHEMA);
+  const [loadData, { called, loading, data, error }] = useLazyQuery(GET_SCHEMA);
+
+  const onClick = () => {
+    loadData();
+    setOpen((prev) => !prev);
+  };
+
+  const content = useMemo(() => {
+    if (!called) {
+      return '';
+    }
+    if (loading) {
+      return 'Loading...';
+    }
+    if (error) {
+      return error.message;
+    }
+    return JSON.stringify(data, null, '\t');
+  }, [called, data, error, loading]);
 
   return (
     <>
-      <div
-        className={`${styles.button} ${open && styles.opened}`}
-        onClick={() => setOpen((prev) => !prev)}
-      >
+      <div className={`${styles.button} ${open && styles.opened}`} onClick={onClick}>
         SCHEMA
       </div>
-      <pre className={`${styles.schema} ${open && styles.opened}`}>
-        {loading ? 'Loading...' : error ? error.message : JSON.stringify(data, null, '\t')}
-      </pre>
+      <pre className={`${styles.schema} ${open && styles.opened}`}>{content}</pre>
     </>
   );
 };
